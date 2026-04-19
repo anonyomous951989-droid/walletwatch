8c65fb5cdc7ea7355cc1dc85cf166d7bae388b2c
RajDevvanshi <anonyomous951989@gmail.com>
2026-04-19 23:34:23 +0530

    backend link

--- client/src/utils/baseURL.js
+++ client/src/utils/baseURL.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://raj-walletwatch.onrender.com";
 //export const BASE_URL =
- // "https://expense-management-system-backend-s156.onrender.com";
+// "https://expense-management-system-backend-s156.onrender.com";

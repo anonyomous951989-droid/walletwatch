281728b088623f9d79301f1a3ac67e4dbe91693d
RajDevvanshi <anonyomous951989@gmail.com>
2026-06-20 14:27:48 +0530

    Update emailConfig.js

--- server/config/emailConfig.js
+++ server/config/emailConfig.js
@@ -1,4 +1,7 @@
 const nodemailer = require("nodemailer");
+console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",

bf9bf1e45b0508221398fb883c06d5c3a86baf56
RajDevvanshi <anonyomous951989@gmail.com>
2026-06-20 14:47:39 +0530

    Update emailConfig.js

--- server/config/emailConfig.js
+++ server/config/emailConfig.js
@@ -4,15 +4,13 @@ console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
+  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 

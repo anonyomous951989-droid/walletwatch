3bb008a3a1eeddd58ec16caed0785c75cca38801
RajDevvanshi <anonyomous951989@gmail.com>
2026-06-20 15:46:43 +0530

    Update emailConfig.js

--- server/config/emailConfig.js
+++ server/config/emailConfig.js
@@ -1,16 +1,29 @@
 const nodemailer = require("nodemailer");
+
 console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
 console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  port: Number(process.env.EMAIL_PORT),
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+
+// ADD THIS TEMPORARILY
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
 });
 
 module.exports = transporter;
